81b53c710a1e0e57bbaeb05ecf98f27e29d211e6
Ryan Makoni <rmakoni@3-G-8.42heilbronn.de>
2025-02-11 15:22:10 +0100

    file structure and param check

---
+++ philo/param_check.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   param_check.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 14:11:15 by rmakoni           #+#    #+#             */
+/*   Updated: 2025/02/11 15:21:39 by rmakoni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_isdigit(int num)
+{
+	if (num >= '0' && num <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+void	error_msg(void)
+{
+	printf("You have entered the incorrect perameters! Please enter:\n");
+	printf("./philo {number_of_philosopher {time_to_die} {time_to_eat} {time_to_sleep} [number_of_times_each_philosopher_must_eat]");
+}
+
+//returns 1 if params are correct and 0 if not
+int	check_params(int argc, char **argv)
+{
+	int	i;
+	int	x;
+
+	i = 1;
+	if (argc == 5 || argc == 6)
+	{
+		while (i <= argc - 1)
+		{
+			x = 0;
+			while (argv[i][x] != '\0')
+			{
+				if (!ft_isdigit(argv[i][x]))
+					return (0);
+				x++;
+			}
+			i++;
+		}
+	}
+	else
+		return (0);
+	return (1);
+}
---
+++ philo/philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 13:06:37 by rmakoni           #+#    #+#             */
+/*   Updated: 2025/02/11 14:21:36 by rmakoni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+typedef struct s_philo
+{
+}		t_philo;
+
+typedef struct s_forks
+{
+}		t__forks;
+
+int		check_params(int argc, char **argv);
+void	error_msg(void);
+#endif
