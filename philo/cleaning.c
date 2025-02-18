2b191fec0b13d523ffc8d3af2f7aaf5e15f83e26
Ryan Makoni <rmakoni@1-D-23.42heilbronn.de>
2025-02-18 13:51:18 +0100

    clean up function

--- philo/cleaning.c
+++ philo/cleaning.c
@@ -6,6 +6,30 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 13:23:44 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/02/18 13:23:48 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/02/18 13:38:11 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "philo.h"
+
+void	clean_up(t_data *data)
+{
+	int	i;
+
+	if (data)
+	{
+		if (data->forks)
+		{
+			i = 0;
+			while (i < data->no_philo)
+			{
+				pthread_mutex_destroy(&data->forks[i]);
+				i++;
+			}
+			free(data->forks);
+		}
+		if (data->philos)
+			free(data->philos);
+		free(data);
+	}
+}
