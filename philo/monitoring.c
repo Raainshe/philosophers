ee9313d20e1583c5b7d726212516e1a512e0e631
Ryan Makoni <rmakoni@1-C-24.42heilbronn.de>
2025-03-11 17:24:00 +0100

    timing changes fixed some stuff

--- philo/monitoring.c
+++ philo/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:44:11 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/03/11 16:41:29 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/03/11 17:12:36 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ void	*death_monitor(void *arg)
 			pthread_mutex_unlock(&data->death_lock);
 			return (NULL);
 		}
-		usleep(500);
+		usleep(200);
 	}
 	return (NULL);
 }
